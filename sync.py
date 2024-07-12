674ad450a4ad218fd7e62d9e2d06769b53c6de5d
harper25 <olesjakubb@gmail.com>
2024-07-12 16:32:23 +0200

    add more classes - wdyt?

--- sync.py
+++ sync.py
@@ -12,15 +12,17 @@ def sync(source, dest):
     # step 2: call functional core
     actions = determine_actions(source_hashes, dest_hashes, source, dest)
 
-    # imperative shell step 3, apply outputs
-    for action, *paths in actions:
-        if action == "COPY":
-            shutil.copyfile(*paths)
-        if action == "MOVE":
-            shutil.move(*paths)
-        if action == "DELETE":
-            os.remove(paths[0])
+    # # imperative shell step 3, apply outputs
+    # for action, *paths in actions:
+    #     if action == "COPY":
+    #         shutil.copyfile(*paths)
+    #     if action == "MOVE":
+    #         shutil.move(*paths)
+    #     if action == "DELETE":
+    #         os.remove(paths[0])
 
+    for command in actions:
+        command.run()
 
 BLOCKSIZE = 65536
 
@@ -43,18 +45,73 @@ def read_paths_and_hashes(root):
     return hashes
 
 
+# # original
+# def determine_actions(source_hashes, dest_hashes, source_folder, dest_folder):
+#     for sha, filename in source_hashes.items():
+#         if sha not in dest_hashes:
+#             sourcepath = Path(source_folder) / filename
+#             destpath = Path(dest_folder) / filename
+#             yield "COPY", sourcepath, destpath
+
+#         elif dest_hashes[sha] != filename:
+#             olddestpath = Path(dest_folder) / dest_hashes[sha]
+#             newdestpath = Path(dest_folder) / filename
+#             yield "MOVE", olddestpath, newdestpath
+
+#     for sha, filename in dest_hashes.items():
+#         if sha not in source_hashes:
+#             yield "DELETE", dest_folder / filename
+
+
 def determine_actions(source_hashes, dest_hashes, source_folder, dest_folder):
     for sha, filename in source_hashes.items():
         if sha not in dest_hashes:
             sourcepath = Path(source_folder) / filename
             destpath = Path(dest_folder) / filename
-            yield "COPY", sourcepath, destpath
+            yield Copy(sourcepath, destpath)
 
         elif dest_hashes[sha] != filename:
             olddestpath = Path(dest_folder) / dest_hashes[sha]
             newdestpath = Path(dest_folder) / filename
-            yield "MOVE", olddestpath, newdestpath
+            yield Move(olddestpath, newdestpath)
 
     for sha, filename in dest_hashes.items():
         if sha not in source_hashes:
-            yield "DELETE", dest_folder / filename
+            yield Delete(dest_folder / filename)
+
+
+class Command:
+    def run(self):
+        pass
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.__dict__ == other.__dict__
+        else:
+            return False
+
+
+class Copy(Command):
+    def __init__(self, source, dest):
+        self.source = source
+        self.dest = dest
+
+    def run(self):
+        shutil.copyfile(self.source, self.dest)
+
+
+class Move(Command):
+    def __init__(self, source, dest):
+        self.source = source
+        self.dest = dest
+
+    def run(self):
+        shutil.move(self.source, self.dest)
+
+
+class Delete(Command):
+    def __init__(self, path):
+        self.path = path
+
+    def run(self):
+        os.remove(self.path)
--- test_sync.py
+++ test_sync.py
@@ -1,7 +1,7 @@
 import tempfile
 from pathlib import Path
 import shutil
-from sync import sync, determine_actions
+from sync import sync, determine_actions, Copy, Move, Delete
 
 
 class TestE2E:
@@ -51,11 +51,13 @@ def test_when_a_file_exists_in_the_source_but_not_the_destination():
     source_hashes = {"hash1": "fn1"}
     dest_hashes = {}
     actions = determine_actions(source_hashes, dest_hashes, Path("/src"), Path("/dst"))
-    assert list(actions) == [("COPY", Path("/src/fn1"), Path("/dst/fn1"))]
+    # assert list(actions) == [("COPY", Path("/src/fn1"), Path("/dst/fn1"))]
+    assert list(actions) == [Copy(Path("/src/fn1"), Path("/dst/fn1"))]
 
 
 def test_when_a_file_has_been_renamed_in_the_source():
     source_hashes = {"hash1": "fn1"}
     dest_hashes = {"hash1": "fn2"}
     actions = determine_actions(source_hashes, dest_hashes, Path("/src"), Path("/dst"))
-    assert list(actions) == [("MOVE", Path("/dst/fn2"), Path("/dst/fn1"))]
+    # assert list(actions) == [("MOVE", Path("/dst/fn2"), Path("/dst/fn1"))]
+    assert list(actions) == [Move(Path("/dst/fn2"), Path("/dst/fn1"))]
